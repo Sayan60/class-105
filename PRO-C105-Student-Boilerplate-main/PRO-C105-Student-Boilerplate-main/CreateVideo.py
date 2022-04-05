d0b1a5d81b52037ca24799d163fe7b81634a62c4
Sayan60 <72643308+Sayan60@users.noreply.github.com>
2022-04-05 19:54:28 +0530

    Add files via upload

---
+++ PRO-C105-Student-Boilerplate-main/PRO-C105-Student-Boilerplate-main/CreateVideo.py
@@ -0,0 +1,47 @@
+import os
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+print(count)
+
+frame=cv2.imread(images[0])
+height,width,channels=frame.shape
+size=(width,height)
+
+out = cv2.VideoWriter('project1.mp4',cv2.VideoWriter_fourcc(*'DIVX'),5,size)
+
+#sunrise
+#for i in range(count-1,0,-1):
+
+#sunset
+for i in range(0,count-1):
+    frame=cv2.imread(images[i])
+    out.write(frame)
+
+out.release()
+print("done")    
+
+
+
+
+
+
+
+
